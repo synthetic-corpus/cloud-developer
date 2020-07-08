986f8bf2b46488a9aca396230ca6b18b92323d5d
synthetic.corpus <joel@joelgonzaga.com>
2020-07-08 13:52:07 -0700

    first attempt at mocha test.

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, combine } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,11 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('smashstring', () => {
+
+  it('should ouuput "housecat"', () => {
+    const result = combine('house','cat');
+    expect(result).to.equal('housecat')
+  })
+})
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -10,6 +10,10 @@ export const divide = (a: number, b: number) => {
     return a / b;
     }
 
+export const combine = (a: string, b: string) => {
+    return a + b
+}
+
 // @TODO try creating a method "concat" to concatenate two strings
 // it should take two string paramaters.
 // it should return one string combining the two strings.
