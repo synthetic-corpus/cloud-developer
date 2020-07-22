bc73dbd6fb9e55f61f8df04e7c8015035901349f
synthetic.corpus <joel@joelgonzaga.com>
2020-07-22 11:57:51 -0700

    completed bcrypt gen salt files.

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,12 +12,14 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return;
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(plainTextPassword, salt)
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return;
+    return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
