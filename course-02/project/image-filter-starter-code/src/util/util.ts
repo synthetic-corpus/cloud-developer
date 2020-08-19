f4e649e88296122a6d766952ca183b7b85901ab5
synthetic.corpus <joel@joelgonzaga.com>
2020-08-19 14:58:35 -0700

    abandonded feature to validate url with an HTTP request before .Jimp.

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import Jimp = require('jimp');
 import Https = require('https')
+import Request = require('request')
 
 
 
@@ -37,13 +38,8 @@ export async function deleteLocalFiles(files:Array<string>){
     }
 }
 
-function urlExists(address: string): boolean {
-    return true
-}
-/* validate a URL */
-export async function validateImageURL(address: string): boolean{
-    /*
-    // Is it even written correctly?
+// Check if Url is Good
+export function validateImageURL(address: string): boolean{
     try{
         const newAddress = new URL(address)
     }catch(TypeError){
@@ -51,16 +47,6 @@ export async function validateImageURL(address: string): boolean{
     }
     // Is it written like we'd expect an image?
     return address.toLowerCase().match(/(.jpeg|.jpg|.gif|.png)/) != null
-    */
-   
-    //if it looks like it would have an image, validate the URL
-    if(address.toLowerCase().match(/(.jpeg|.jpg|.gif|.png)/) != null) {
-        const value = urlExists(address)//returns false on a 4xx error
-        console.log("**** Got the Returned value of ",value)
-        return value
-    }else{
-        return false
-    }
 }
 
 /* Extracts the Query string and turns it into a URL that can be input into jimp */
